d3b685249aa3d63c6c79100c0713c0285a68ce0f
Muhammad Asif Ansari <muhammadasifansari101@gmail.com>
2024-10-25 11:23:59 +0500

    assignment
    assignment

---
+++ app.js
@@ -0,0 +1,55 @@
+// function add(a,b,c){
+//     console.log(a+b+c);
+// }
+// add(32,56,97);
+
+
+// function sub(a,b,c){
+//     console.log(a-b-c);
+// }
+// sub(32,56,97);
+
+
+// function mod(a,b,c,d){
+//     console.log(a%b%c%d);
+// }
+// mod(22,56,97,98);
+
+
+// function divide(a,b,c,d,e){
+//     console.log(a/b/c/d);
+// }
+// divide(16,23,45,12,17);
+
+
+
+// function divide(a,b,c){
+//     console.log(a*b*c);
+// }
+// divide(12,2,4);
+
+
+// var food = prompt("Enter a food").toLowerCase();
+
+// switch (food) {
+//     case "biryani":
+//     alert("1kg lekr ao")
+//         break;
+//         case "burger":
+//             alert("1 lekr ao")
+//                 break;
+//                 case "nihari":
+//                     alert("1 kg Nihari lekr ao")
+//                         break;
+
+//     default:
+//         alert("wapis ajao");
+//         break;
+// }
+
+
+function hello() {
+    var name = document.getElementById("name");
+    console.log(name.value);
+    name.value
+}
